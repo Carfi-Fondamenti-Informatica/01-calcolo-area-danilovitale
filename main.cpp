cf312801ee052d2cb813bc1a4166b4c6e6d36d6c
danilovitale <115080010+danilovitale@users.noreply.github.com>
2022-11-10 00:02:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main ()
 {
 float a = 0, b = 0, c = 0; //inizializzo le variabili float
-cin >> a >> b >> c; //non eseguo controlli sui valori inseriti, non sebra richiesto dalla consegna dell'esercitazione
+cin >> a >> b >> c; //non eseguo controlli sui valori inseriti, non sembra richiesto dalla consegna dell'esercitazione
 cout<< "\n"<< ((a*b)/2) << endl<< (a*a) << endl<< (a*b) << endl<< (((a+b)*c)/2) << endl; //stampo a video i risultati, andando a capo ad ogni stampa
 return 0;
 }
