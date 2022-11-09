0931026dec4974f9e1f7105994a452fd66788172
danilovitale <115080010+danilovitale@users.noreply.github.com>
2022-11-10 00:02:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 using namespace std;
-
-int main(){
-//Qui vanno definite le variabili
-
-cin >> a >> b >> c;
-
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+int main ()
+{
+float a = 0, b = 0, c = 0; //inizializzo le variabili float
+cin >> a >> b >> c; //non eseguo controlli sui valori inseriti, non sebra richiesto dalla consegna dell'esercitazione
+cout<< "\n"<< ((a*b)/2) << endl<< (a*a) << endl<< (a*b) << endl<< (((a+b)*c)/2) << endl; //stampo a video i risultati, andando a capo ad ogni stampa
+return 0;
 }
